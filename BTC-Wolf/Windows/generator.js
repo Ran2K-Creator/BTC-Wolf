9d9d76fccd01a7c5e819250daaeb4d6c06a382bb
Ran Malamud - Ran2K <65309980+Ran2K-Creator@users.noreply.github.com>
2023-03-26 18:09:44 +0300

    crypto-wolf.js V2.0

--- BTC-Wolf/Windows/generator.js
+++ BTC-Wolf/Windows/generator.js
@@ -1,69 +1,74 @@
-"use strict";
+'use strict';
 
 const CoinKey = require('coinkey');
 const fs = require('fs');
+const { Worker, isMainThread } = require('worker_threads');
+const { randomBytes } = require('crypto');
+const BloomFilter = require('bloom-filter');
 
-let privateKeyHex, ck, addresses;
-addresses = new Map();
+const ADDRESS_FILE_PATH = './sample-addresses.txt';
+const SUCCESS_FILE_PATH = './Success.txt';
+const BATCH_SIZE = 1000;
 
-const data = fs.readFileSync('./sample-addresses.txt');
-data.toString().split("\n").forEach(address => addresses.set(address, true));
-
+let privateKeyHex, ck, filter, addresses, successQueue;
 let count = 0;
-const startTime = Date.now();
 
 function generate() {
-    // generate random private key hex
-    let privateKeyHex = r(64);
-    
-    // create new bitcoin key pairs
-    let ck = new CoinKey(Buffer.from(privateKeyHex, 'hex'));
-    
-    ck.compressed = false;
-    //console.log(ck.publicAddress)
-    // remove the 
-        
-    // if generated wallet matches any from the riches.txt file, tell us we won!
-    if(addresses.has(ck.publicAddress)){
-        console.log("");
-        process.stdout.write('\x07');
-        console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress);
-        var successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif;
-            
-        // save the wallet and its private key (seed) to a Success.txt file in the same folder 
-        fs.writeFileSync('./Success.txt', successString, (err) => {
-            if (err) throw err; 
-        })
-            
-        // close program after success
-        process.exit();
+  // generate random private key hex
+  const privateKeyBytes = randomBytes(32);
+  const privateKeyHex = privateKeyBytes.toString('hex');
+
+  // create new bitcoin key pairs
+  const ck = new CoinKey(privateKeyBytes);
+
+  ck.compressed = false;
+
+  // check if generated wallet matches any from the riches.txt file
+  if (filter.contains(ck.publicKey)) {
+    console.log('');
+    process.stdout.write('\x07');
+    console.log('\x1b[32m%s\x1b[0m', `>> Success: ${ck.publicAddress}`);
+
+    // add the wallet and its private key (seed) to the success queue
+    successQueue.push({ address: ck.publicAddress, privateKey: ck.privateWif });
+
+    if (successQueue.length >= BATCH_SIZE) {
+      writeSuccessFile();
     }
-    // destroy the objects
-    ck = null;
-    privateKeyHex = null;
-    
-    count++;
-    process.title = `${count}  checked`;
+
+    // close program after success
+    process.exit();
+  }
+
+  count++;
+  process.title = `${count} checked`;
 }
 
-// the function to generate random hex string
-function r(l) {
-    let randomChars = 'ABCDEF0123456789';
-    let result = '';
-    for ( var i = 0; i < l; i++ ) {
-        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+function writeSuccessFile() {
+  const results = successQueue.map((result) => `Wallet: ${result.address}\n\nSeed: ${result.privateKey}\n`);
+  fs.appendFileSync(SUCCESS_FILE_PATH, results.join('\n'), (err) => {
+    if (err) {
+      console.error(`Error writing to ${SUCCESS_FILE_PATH}: ${err.message}`);
     }
-    return result;
+  });
+  successQueue = [];
 }
 
-console.log("\x1b[32m%s\x1b[0m", "--> Started the program in the background, to see the proccess go to line 23"); // don't trip, it works
+if (isMainThread) {
+  console.log('\x1b[32m%s\x1b[0m', '--> Started the program in the background');
+  const data = fs.readFileSync(ADDRESS_FILE_PATH);
+  const addressList = data.toString().split('\n').filter((line) => !!line.trim());
 
-// run forever
-while(true){
+  filter = BloomFilter.create(addressList.length, 0.001);
+  addressList.forEach((address) => filter.insert(Buffer.from(address, 'base64')));
+
+  successQueue = [];
+
+  const worker = new Worker(__filename);
+  worker.on('error', (err) => console.error(`Worker error: ${err.message}`));
+  worker.on('exit', (code) => console.log(`Worker exited with code ${code}`));
+} else {
+  while (true) {
     generate();
-    if (process.memoryUsage().heapUsed / 1000000 > 500) {
-        global.gc();
-    }
+  }
 }
-
-
